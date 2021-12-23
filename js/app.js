b4b1f1a890e984de0c6a64183da6f7c91ed296f8
Nayeon Shin <nayeon.codes@gmail.com>
2021-12-23 20:20:04 +0900

    feat: Let users save their work

--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@ const canvas = document.querySelector(".js-canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".js-colors__color");
 const range = document.querySelector(".js-range");
-const mode = document.querySelector(".buttons__fill");
+const modeButton = document.querySelector(".js-buttons__fill");
+const saveButton = document.querySelector(".js-buttons__save");
 
-const INITIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c"; // black
 
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-ctx.strokeStyle = INITIAL_COLOR; // black
+// Sets initial canvas' background color to white
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
@@ -27,14 +32,26 @@ function handleColorClick(event) {
   ctx.fillStyle = color;
 }
 
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "vanilla-paint";
+  link.click();
+}
+
 function handleModeClick() {
   if (isFilling) {
     isFilling = false;
-    mode.innerText = "Fill";
+    modeButton.innerText = "Fill";
   } else {
     isFilling = true;
-    mode.innerText = "Paint";
+    modeButton.innerText = "Paint";
   }
+}
+
+function handleContextMenu(event) {
+  event.preventDefault();
 }
 
 function handleCanvasClick() {
@@ -71,8 +88,10 @@ canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
 canvas.addEventListener("click", handleCanvasClick);
+canvas.addEventListener("contextmenu", handleContextMenu);
 
-mode.addEventListener("click", handleModeClick);
+modeButton.addEventListener("click", handleModeClick);
+saveButton.addEventListener("click", handleSaveClick);
 
 colors.forEach((color) => color.addEventListener("click", handleColorClick));
 range.addEventListener("input", handleRangeChange);
